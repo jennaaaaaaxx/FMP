379208a082259b0a919166ab528392ea723c7920
jennaaaaaaxx <146724085+jennaaaaaaxx@users.noreply.github.com>
2024-05-19 23:11:10 +0100

    Bugfixes & camera bounds

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -11,6 +11,14 @@ public class CameraController : MonoBehaviour
 
     private static bool cameraExists;
 
+    public BoxCollider2D boundBox;
+    private Vector3 minBounds;
+    private Vector3 maxBounds;
+
+    private Camera theCamera;
+    private float halfHeight;
+    private float halfWidth;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -23,6 +31,13 @@ public class CameraController : MonoBehaviour
         {
             Destroy(gameObject);
         }
+
+        minBounds = boundBox.bounds.min;
+        maxBounds = boundBox.bounds.max;
+
+        theCamera = GetComponent<Camera>();
+        halfHeight = theCamera.orthographicSize;
+        halfWidth = halfHeight * Screen.width / Screen.height;
     }
 
     // Update is called once per frame
@@ -30,5 +45,24 @@ public class CameraController : MonoBehaviour
     {
         targetPos = new Vector3(followTarget.transform.position.x, followTarget.transform.position.y, transform.position.z);
         transform.position = Vector3.Lerp(transform.position, targetPos, moveSpeed * Time.deltaTime);
+
+        if(boundBox == null)
+        {
+            boundBox = FindObjectOfType<Bounds>().GetComponent<BoxCollider2D>();
+            minBounds = boundBox.bounds.min;
+            maxBounds = boundBox.bounds.max;
+        }
+
+        float clampedX = Mathf.Clamp(transform.position.x, minBounds.x + halfWidth, maxBounds.x - halfWidth);
+        float clampedY = Mathf.Clamp(transform.position.y, minBounds.y + halfHeight, maxBounds.y - halfHeight);
+        transform.position = new Vector3(clampedX, clampedY, transform.position.z);
+    }
+
+    public void SetBounds(BoxCollider2D newBounds)
+    {
+        boundBox = newBounds;
+
+        minBounds = boundBox.bounds.min;
+        maxBounds = boundBox.bounds.max;
     }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,13 +7,14 @@ public class PlayerController : MonoBehaviour
 
     public float moveSpeed;
     private float currentMoveSpeed;
-    public float diagonalMoveModifier;
+    // public float diagonalMoveModifier;
 
     private Animator anim;
     private Rigidbody2D myRigidbody;
 
     private bool playerMoving;
     public Vector2 lastMove;
+    private Vector2 moveInput;
 
     private static bool playerExists;
 
@@ -38,6 +39,8 @@ public class PlayerController : MonoBehaviour
         }
 
         canMove = true;
+
+        lastMove = new Vector2(0, -1f);
     }
 
     // Update is called once per frame
@@ -52,7 +55,7 @@ public class PlayerController : MonoBehaviour
             return;
         }
 
-        if (Input.GetAxisRaw("Horizontal") > 0.5f || Input.GetAxisRaw("Horizontal") < -0.5f)
+        /* if (Input.GetAxisRaw("Horizontal") > 0.5f || Input.GetAxisRaw("Horizontal") < -0.5f)
         {
             // transform.Translate(new Vector3(Input.GetAxisRaw("Horizontal") * moveSpeed * Time.deltaTime, 0f, 0f));
             myRigidbody.velocity = new Vector2(Input.GetAxisRaw("Horizontal") * currentMoveSpeed, myRigidbody.velocity.y);
@@ -76,16 +79,29 @@ public class PlayerController : MonoBehaviour
         if(Input.GetAxisRaw("Vertical") < 0.5f && Input.GetAxisRaw("Vertical") > -0.5f)
         {
             myRigidbody.velocity = new Vector2(myRigidbody.velocity.x, 0f);
+        } */
+
+        moveInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")).normalized;
+        
+        if(moveInput != Vector2.zero)
+        {
+            myRigidbody.velocity = new Vector2 (moveInput.x * moveSpeed, moveInput.y * moveSpeed);
+            playerMoving = true;
+            lastMove = moveInput;
+        }
+        else
+        {
+            myRigidbody.velocity = Vector2.zero;
         }
 
-        if(Mathf.Abs(Input.GetAxisRaw("Horizontal")) > 0.5f && Mathf.Abs(Input.GetAxisRaw("Vertical")) > 0.5f)
+        /* if(Mathf.Abs(Input.GetAxisRaw("Horizontal")) > 0.5f && Mathf.Abs(Input.GetAxisRaw("Vertical")) > 0.5f)
         {
             currentMoveSpeed = moveSpeed * diagonalMoveModifier;
         }
         else
         {
             currentMoveSpeed = moveSpeed;
-        }
+        } */
 
         anim.SetFloat("MoveX", Input.GetAxisRaw("Horizontal"));
         anim.SetFloat("MoveY", Input.GetAxisRaw("Vertical"));
